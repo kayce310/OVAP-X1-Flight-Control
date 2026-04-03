6ef16f7fab0e6997805ce349e4845df348dfefc7
Kayce <kayce.nguyen.310203@gmail.com>
2026-04-03 13:35:40 +0700

    feat(OVAP-X1): Phiên bản demo đầu tiên kiểm chứng WPIN và mô hình hệ thống

--- _01_Changing/utils/saturate.m
+++
@@ -1,6 +0,0 @@
-function val_out = saturate(val_in, min_val, max_val)
-% File Name: saturate.m
-% Position: Root > utils > saturate.m
-% Description: Clamps a value or vector between min and max limits.
-    val_out = max(min_val, min(max_val, val_in));
-end
--- _01_Changing/utils/skew_symmetric.m
+++
@@ -1,8 +0,0 @@
-function S = skew_symmetric(v)
-% File Name: skew_symmetric.m
-% Position: Root > utils > skew_symmetric.m
-% Description: Returns the skew-symmetric matrix for cross product equivalent.
-    S = [  0,    -v(3),  v(2);
-           v(3),  0,    -v(1);
-          -v(2),  v(1),  0   ];
-end
